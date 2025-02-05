357e8cc9dee65183d3cfecd4a2f41c1092923594
Randall Bennett <randall@boltfoundry.com>
2025-02-05 19:21:48 +0000

    Remove personSettings page
    Summary:
    
    Test Plan:

---
+++ packages/app/__generated__/__isograph/BfCurrentViewerLoggedOut/AuthenticationOptions/param_type.ts
@@ -0,0 +1,10 @@
+
+export type BfCurrentViewerLoggedOut__AuthenticationOptions__param = {
+  readonly data: {
+    /**
+Unique identifier for the resource
+    */
+    readonly id: string,
+  },
+  readonly parameters: Record<PropertyKey, never>,
+};
