6f6083f7bc0590a858797ec223d120022d7fdee9
Randall Bennett <randall@boltfoundry.com>
2025-02-05 18:56:04 +0000

    Fix db
    Summary:
    
    Test Plan:

--- infra/bff/friends/createPerson.bff.ts
+++ infra/bff/friends/createPerson.bff.ts
@@ -3,6 +3,8 @@ import { getLogger } from "packages/logger.ts";
 import { generateBluey } from "lib/generateBluey.ts";
 import { BfCurrentViewer } from "packages/bfDb/classes/BfCurrentViewer.ts";
 import { BfPerson } from "packages/bfDb/models/BfPerson.ts";
+import { toBfGid } from "packages/bfDb/classes/BfNodeIds.ts";
+import { generateUUID } from "lib/generateUUID.ts";
 const logger = getLogger(import.meta);
 
 register(
@@ -10,10 +12,10 @@ register(
   "Creates a person in the system and returns a url to register",
   async ([name]) => {
     logger.info("creating a person now.");
-    const cv = BfCurrentViewer.__DANGEROUS_USE_IN_SCRIPTS_ONLY__createOmni(
-      import.meta,
-    );
-    const person = await BfPerson.create(cv, { name });
+    const cv = BfCurrentViewer.__DANGEROUS_USE_IN_SCRIPTS_ONLY__createOmni(import.meta);
+    const bfGid = toBfGid(generateUUID());
+    const metadata = { bfGid, bfOid: bfGid }
+    const person = await BfPerson.__DANGEROUS__createUnattached(cv, { name }, metadata);
 
     logger.info(generateBluey(
       `Successfully created the user! Go to:
--- packages/bfDb/classes/BfCurrentViewer.ts
+++ packages/bfDb/classes/BfCurrentViewer.ts
@@ -3,27 +3,83 @@ import { BfGid, toBfGid } from "packages/bfDb/classes/BfNodeIds.ts";
 
 const logger = getLogger(import.meta);
 
-/** not done */
-export class BfCurrentViewer {
+export abstract class BfCurrentViewer {
+  __typename: string;
   static async createFromRequest(importMeta: ImportMeta, _request: Request) {
-    logger.error("Currently using omniCV for everything. This is bad™")
-    return this.__DANGEROUS_USE_IN_SCRIPTS_ONLY__createOmni(importMeta)
+    const cv = BfCurrentViewerLoggedOut
+      .__PROBABLY_DONT_USE_THIS_VERY_OFTEN__create(importMeta);
+    return cv;
   }
   static __DANGEROUS_USE_IN_SCRIPTS_ONLY__createOmni(importMeta: ImportMeta) {
     logger.warn(`Creating omnivc from: ${importMeta.url}`);
-    return new BfCurrentViewer__DANGEROUS__OMNI__(importMeta, toBfGid("OMNI"), toBfGid("OMNI"));
+    return BfCurrentViewer__DANGEROUS__OMNI__
+      .__PROBABLY_DONT_USE_THIS_VERY_OFTEN__create(importMeta);
+  }
+  static __DANGEROUS_USE_IN_SCRIPTS_ONLY__createLoggedIn(
+    importMeta: ImportMeta,
+    bfGid: string | BfGid,
+    bfOid: string | BfGid,
+  ) {
+    logger.warn(`Creating Logged in user: ${importMeta.url}`);
+    return BfCurrentViewerLoggedIn.__PROBABLY_DONT_USE_THIS_VERY_OFTEN__create(
+      importMeta,
+      toBfGid(bfGid),
+      toBfGid(bfOid),
+    );
+
+
+  }
+
+  static createLoggedOut(
+    importMeta: ImportMeta,
+  ) {
+    logger.warn(`Creating Logged in user: ${importMeta.url}`);
+    return BfCurrentViewerLoggedOut.__PROBABLY_DONT_USE_THIS_VERY_OFTEN__create(
+      importMeta,
+    );
   }
   clear() {}
-
-  __typename = this.constructor.name;
 
   protected constructor(
     readonly creator: ImportMeta, // the import.meta of the module that created the current viewer
     readonly bfGid: BfGid, // person for whom the access token was created
     readonly bfOid: BfGid, // always an owner, used to determine access control
   ) {
+    this.__typename = this.constructor.name;
+  }
+
+  toGraphql() {
+    return { __typename: this.constructor.name, id: this.bfGid };
+  }
+
+  toString() {
+    return `${this.constructor.name}#${this.bfGid}⚡️${this.bfOid}`;
   }
 }
 
-export class BfCurrentViewer__DANGEROUS__OMNI__ extends BfCurrentViewer {
+export class BfCurrentViewerLoggedOut extends BfCurrentViewer {
+  protected constructor(
+    override readonly creator: ImportMeta, // the import.meta of the module that created the current viewer
+  ) {
+    super(creator, toBfGid("ANON"), toBfGid("ANON"));
+  }
+
+  static __PROBABLY_DONT_USE_THIS_VERY_OFTEN__create(creator: ImportMeta) {
+    return new this(creator);
+  }
+}
+class BfCurrentViewerLoggedIn extends BfCurrentViewer {
+  static __PROBABLY_DONT_USE_THIS_VERY_OFTEN__create(
+    creator: ImportMeta,
+    bfGid: BfGid,
+    bfOid: BfGid,
+  ) {
+    return new this(creator, bfGid, bfOid);
+  }
+}
+
+class BfCurrentViewer__DANGEROUS__OMNI__ extends BfCurrentViewer {
+  static __PROBABLY_DONT_USE_THIS_VERY_OFTEN__create(creator: ImportMeta) {
+    return new this(creator, toBfGid("OMNI"), toBfGid("OMNI"));
+  }
 }
