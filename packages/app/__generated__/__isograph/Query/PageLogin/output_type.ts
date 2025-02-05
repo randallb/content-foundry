61c6f6eba20769600b2c021531040394e2793eb2
Randall Bennett <randall@boltfoundry.com>
2025-02-05 15:52:06 +0000

    Add login route
    Summary:
    
    Scaffolds the login route
    
    Test Plan:
    
    https://edf8e625-97b2-43e7-91e8-885e54ecef55-00-20ykhnvy56b3p.kirk.replit.dev/login

---
+++ packages/app/__generated__/__isograph/Query/PageLogin/output_type.ts
@@ -0,0 +1,4 @@
+import type { ExtractSecondParam, CombineWithIntrinsicAttributes } from '@isograph/react';
+import type React from 'react';
+import { PageLogin as resolver } from '../../../../pages/PageLogin.tsx';
+export type Query__PageLogin__output_type = (React.FC<CombineWithIntrinsicAttributes<ExtractSecondParam<typeof resolver>>>);
---
+++ packages/app/__generated__/__isograph/Query/PageLogin/resolver_reader.ts
@@ -0,0 +1,34 @@
+import type {ComponentReaderArtifact, ExtractSecondParam, ReaderAst } from '@isograph/react';
+import { Query__PageLogin__param } from './param_type.ts';
+import { PageLogin as resolver } from '../../../../pages/PageLogin.tsx';
+
+const readerAst: ReaderAst<Query__PageLogin__param> = [
+  {
+    kind: "Linked",
+    fieldName: "me",
+    alias: null,
+    arguments: null,
+    condition: null,
+    selections: [
+      {
+        kind: "Scalar",
+        fieldName: "__typename",
+        alias: null,
+        arguments: null,
+      },
+    ],
+  },
+];
+
+const artifact: ComponentReaderArtifact<
+  Query__PageLogin__param,
+  ExtractSecondParam<typeof resolver>
+> = {
+  kind: "ComponentReaderArtifact",
+  componentName: "Query.PageLogin",
+  resolver,
+  readerAst,
+  hasUpdatable: false,
+};
+
+export default artifact;
