e7f848d174b7e59f41faf103a9eedde74c1b11ba
Randall Bennett <randall@boltfoundry.com>
2025-02-07 04:16:59 +0000

    page scaffolds
    Summary:
    
    Scaffolds out the page structure.
    
    Test Plan:
    
    ![img](https://github.com/user-attachments/assets/873bf904-d000-4567-bf4a-476ffd7daf21)

---
+++ packages/app/__generated__/__isograph/Query/RegistrationOptions/output_type.ts
@@ -0,0 +1,3 @@
+import type React from 'react';
+import { RegistrationOptions as resolver } from '../../../../entrypoints/EntrypointRegistrationOptions.tsx';
+export type Query__RegistrationOptions__output_type = ReturnType<typeof resolver>;
