88f50ea13d90b7d6e57fc93b1a62da659f666862
runner@73066a9f3262 <>
2025-02-11 11:24:22 +0000

    Deno 2.1.4 changes
    Summary:
    
    Test Plan:

--- packages/app/__generated__/__isograph/BfBlogPost/BlogPostListItem/param_type.ts
+++
@@ -1,9 +0,0 @@
-
-export type BfBlogPost__BlogPostListItem__param = {
-  readonly data: {
-    readonly __typename: string,
-    readonly author: (string | null),
-    readonly content: (string | null),
-  },
-  readonly parameters: Record<PropertyKey, never>,
-};
--- packages/app/__generated__/__isograph/Query/Blog/resolver_reader.ts
+++ packages/app/__generated__/__isograph/Query/Blog/resolver_reader.ts
@@ -1,7 +1,6 @@
 import type {ComponentReaderArtifact, ExtractSecondParam, ReaderAst } from '@isograph/react';
 import { Query__Blog__param } from './param_type.ts';
 import { Blog as resolver } from '../../../../components/Blog/Blog.tsx';
-import BfBlog__BlogPostList__resolver_reader from '../../BfBlog/BlogPostList/resolver_reader.ts';
 
 const readerAst: ReaderAst<Query__Blog__param> = [
   {
@@ -9,22 +8,6 @@ const readerAst: ReaderAst<Query__Blog__param> = [
     fieldName: "__typename",
     alias: null,
     arguments: null,
-  },
-  {
-    kind: "Linked",
-    fieldName: "blog",
-    alias: null,
-    arguments: null,
-    condition: null,
-    selections: [
-      {
-        kind: "Resolver",
-        alias: "BlogPostList",
-        arguments: null,
-        readerArtifact: BfBlog__BlogPostList__resolver_reader,
-        usedRefetchQueries: [],
-      },
-    ],
   },
 ];
 
