f753f2cc0663058a0501ccb66e95decabc58bfa2
runner@810cf758011d <>
2025-02-13 01:42:26 +0000

    BfDocs frontend
    Summary:
    
    Test Plan:

--- packages/app/entrypoints/EntrypointDocs.tsx
+++ packages/app/entrypoints/EntrypointDocs.tsx
@@ -1,19 +1,23 @@
+
 import { iso } from "packages/app/__generated__/__isograph/iso.ts";
 import type { RouteEntrypoint } from "packages/app/routes.ts";
-import { useRouter } from "packages/app/contexts/RouterContext.tsx";
 
 export const EntrypointDocs = iso(`
   field Query.EntrypointDocs {
     me {
       docs {
-        name
+        DocsList
       }
-      Docs
     }
   }
 `)(function EntrypointDocs({ data }): RouteEntrypoint {
-  const title = "Documentation";
-  const DefaultBody = () => "Loading documentation...";
-
-  return { Body: data?.me?.Docs ?? DefaultBody, title: data?.me?.docs?.name ?? title };
+  const DocsList = data?.me?.docs?.DocsList;
+  return {
+    Body: () => (
+      <div className="docs-container">
+        {DocsList ? <DocsList /> : <div>DocsList docs not found</div>}
+      </div>
+    ),
+    title: "Documentation",
+  };
 });
