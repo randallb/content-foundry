5a5a3e7e564d45062002707c6302a48fb5a1297f
runner@41c04512b593 <>
2025-02-14 13:52:22 +0000

    Update blog post list item to use content from remote stuff
    Summary:
    
    Test Plan:

--- packages/app/components/BfBlogPost/BlogPostListItem.tsx
+++ packages/app/components/BfBlogPost/BlogPostListItem.tsx
@@ -1,4 +1,36 @@
 import { iso } from "packages/app/__generated__/__isograph/iso.ts";
+import { use } from "react";
+import { getLogger } from "packages/logger.ts";
+const _logger = getLogger(import.meta);
+
+const loadingPromises = new Map<string, Promise<React.FC>>();
+
+function getComponent(
+  path: string,
+): Promise<React.FC> {
+  if (loadingPromises.has(path)) {
+    return loadingPromises.get(path)!;
+  }
+  let gettablePath = `build/content${path}`;
+  if (typeof Deno === "undefined") {
+    const regexForMdAndMdx = /\.mdx?$/;
+    gettablePath = `/static/${gettablePath.replace(regexForMdAndMdx, ".js")}`;
+  }
+  const nextPromise = new Promise<React.FC>((resolve) => {
+    import(gettablePath).then((module) => {
+      resolve(module.default as React.FC);
+    });
+  });
+  loadingPromises.set(path, nextPromise);
+  return nextPromise;
+}
+
+function useContent(path: string | null, showContent: boolean) {
+  if (path && showContent) {
+    const Component = use(getComponent(path));
+    return <Component />;
+  }
+}
 
 export const BlogPostListItem = iso(`
   field BfBlogPost.BlogPostListItem @component {
@@ -9,15 +41,23 @@ export const BlogPostListItem = iso(`
     summary
     href
   }
-`)(function BlogPostListItem({ data }) {
+`)(function BlogPostListItem({ data }, { showContent }) {
+  const contentElement = useContent(data.href, showContent);
   return (
-    <a href={data?.href ?? "/"} className="blog-page-list-item" style={{ textDecoration: 'none', color: 'inherit' }}>
-      <div className="blog-page-item-info">
-        <h3>{data.title}</h3>
-        <p>{data.summary}</p>
-        <p>By {data.author}</p>
-        <p>{data.cta}</p>
-      </div>
-    </a>
+    <>
+      <a
+        href={data?.href ?? "/"}
+        className="blog-page-list-item"
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <div className="blog-page-item-info">
+          <h3>{data.title}</h3>
+          <p>{data.summary}</p>
+          <p>By {data.author}</p>
+          {contentElement}
+          <p>{data.cta}</p>
+        </div>
+      </a>
+    </>
   );
 });
